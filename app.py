2e76a40eb09198b07e5c0a0b091f5c599fa85e45
Fiza Khan <fiza1892003@gmail.com>
2023-09-17 18:34:06 +0530

    added more

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import pandas as pd
+from flask_cors import CORS
+import matplotlib.pyplot as plt
+
 
 app= Flask(__name__)
+CORS(app)
 
 df = pd.read_csv('Cleaned.csv')
+
+
 
 @app.route('/get_mean_salary')
 def get_mean_salary():
      
-     mean_salary = df['Salary'].mean()
-     return jsonify({'mean_salary' : mean_salary})
+     gender_pay_gap = df['Salary'].mean()
+    
+     return jsonify({'gender_pay_gap' : gender_pay_gap})
 
 if __name__ == '__main__':
-     from waitress import serve
-     serve(app, host="0.0.0.0", port=8080)
-     app.run()
+     app.run(debug=True,port=5500)
